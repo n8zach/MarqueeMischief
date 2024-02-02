1968b0a8ed569574aa56bb7add588d5c3268af6f
Gregor <gregor@redetech.com>
2024-02-01 19:06:59 -0800

    added saving and voting

--- flask_app.py
+++ flask_app.py
@@ -4,16 +4,25 @@ from json import decoder
 from werkzeug.datastructures import MultiDict 
 from flask import Flask, redirect, render_template, request, url_for, jsonify
 from scrabble import suggest_words
-from mm_game_db import save_answer_by_puzzle_text, get_answers
+from mm_game_db import save_answer_by_puzzle_text, get_results, get_all_answers, get_answers, get_puzzles_with_answers, vote_for_answer
 from db import db
 USE_PROXY = True
+LOCAL = True
 
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="Gregor",
-    password="gaspit",
-    hostname="localhost",
-    databasename="marquee_mischief",
-)
+if(LOCAL):
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+        username="Gregor",
+        password="gaspit",
+        hostname="localhost",
+        databasename="marquee_mischief",
+    )
+else:
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+        username="mischi3f",
+        password="770Intermsnet",
+        hostname="mischi3f.mysql.pythonanywhere-services.com",
+        databasename="mischi3f$MarqueeMahem",
+    )
 
 app = Flask(__name__, template_folder='templates', static_url_path='/static')
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
@@ -22,6 +31,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 with app.app_context():
     db.init_app(app) # to add the app inside SQLAlchemy()
+    #db.create_all()
 
 @app.route('/scrabbler/', methods = ['POST', 'GET'])
 def scrabbler():
@@ -35,15 +45,39 @@ def scrabbler():
     else:
         return redirect(url_for('home'))
 
-answers = []
+@app.route('/vote/', methods = ['POST', 'GET'])
+def vote():
+    if request.method == "GET":
+        puzzles = get_puzzles_with_answers()
+        return render_template("Vote.html", puzzles=puzzles, answers=[], selected=0)
+    
+    # POST
+    # vote or choose puzzle?
+    if(request.form.get("vote")):
+        puzzles = get_puzzles_with_answers()
+        vote_for_answer(userId=1, answerId=request.form["vote"])
+        answers = get_results(request.form["selected"])
+        return render_template("Vote.html", puzzles=puzzles, answers=answers, selected=request.form["selected"])
+    elif(request.form.get("puzzles")):
+        puzzles = get_puzzles_with_answers()
+        answers = get_results(request.form["puzzles"])
+        return render_template("Vote.html", puzzles=puzzles, answers=answers, selected=request.form["puzzles"])
+
 
 @app.route('/test/', methods = ['POST', 'GET'])
 def test():
     if request.method == "GET":
-        result = get_answers()
-        return render_template("GameTest.html", answers=result)
- 
-    save_answer_by_puzzle_text(request.form["answer"], request.form["puzzle"])
+        answers = get_all_answers()
+        results = get_results(9)
+        return render_template("GameTest.html", answers=answers, results=results)
+    
+    # is this a vote?
+    if(request.form.get("vote")):
+        result = vote_for_answer(userId=1, answerId=request.form["vote"])
+    # must be a save
+    else:
+        result = save_answer_by_puzzle_text(request.form["answer"], request.form["puzzle"])
+
     return redirect(url_for('test'))
 
 @app.route('/', methods = ['GET'])
@@ -52,8 +86,8 @@ def default():
 
 @app.route('/save/', methods = ['POST'])
 def save():
-    save_answer_by_puzzle_text(request.form["answer"], request.form["puzzle"])
-    return ""
+    result = save_answer_by_puzzle_text(request.form["answer"], request.form["puzzle"])
+    return result
 
 
 @app.route('/home/', methods = ['POST', 'GET'])
@@ -83,20 +117,23 @@ def thinking():
 
     data = {}
     data["OriginalMessage"] = form_data.getlist('OriginalMessage')[0]
+    close = [x for x in messages["bad"] if len(x["extra"]) == 1]
+
     out = []
-    if(len(messages["good"]) != 0):
+    if(len(messages["good"]) == 0 and len(close) == 0):
+        out.append("<b>I got no suggestions...  sorry :(")
+    elif(len(messages["good"]) != 0):
         out.append("<b>Here are some suggestions:</b>")
         for g in messages["good"]:
             out.append(f'<div onclick="changeTryItBox(this)">{g["text"]}</div>')
     else:
         out.append("<b>I got nothin perfect.</b><br>")
     
-    close = [x for x in messages["bad"] if len(x["extra"]) == 1]
     if(len(close) != 0):
         out.append("<br><b>These are close (Missing One Letter):</b>")
         for b in close:
             out.append(f'<div onclick="changeTryItBox(this)">{format_extra_letters(b["text"], message.upper())}</div>')
-    else:
+    elif(len(messages["good"]) != 0):
         out.append("<b>I got nothin close.</b>")
 
     data["NewMessages"] = ''.join(out)
--- mm_game_db.py
+++ mm_game_db.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, timezone
-from sqlalchemy import DateTime, text
+from sqlalchemy import DateTime, text, insert
 from db import db
 
 class Users(db.Model):
@@ -24,10 +24,29 @@ class Answers(db.Model):
     puzzle = db.relationship("Puzzles", backref="puzzles")
     text = db.Column(db.String(256))
 
+class Votes(db.Model):
+    __tablename__ = "votes"
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship("Users", backref="users")
+    answerId = db.Column(db.Integer, db.ForeignKey('answers.id'))
+
+def vote_for_answer(userId, answerId):
+    sql = f"INSERT INTO votes (userId, answerId) VALUES ({userId}, {answerId})"
+    db.session.execute(text(sql))
+    db.session.commit()
+    #stmt = insert(Votes).values(userId=f"{userId}", answerId=f"{answerId}")
+    #db.session.add(stmt)
+    #db.session.commit()
+    return
+
+
+
 def save_answer_by_puzzle_text(answer_text, puzzle_text):
-    #check if this puzzle already exists
-    #sql = "SELECT * FROM MY_TABLE WHERE text = {puzzle}"
-    puzzles = Puzzles.query.where(Puzzles.text == puzzle_text).all()
+    #puzzles = Puzzles.query.where(Puzzles.text == puzzle_text).all()
+    sql = f"select * from puzzles where puzzles.text = \"{puzzle_text}\";"
+    puzzles = db.session.execute(text(sql)).all()
+
     if (len(puzzles) == 0):
         #save this new puzzle
         puzzle = Puzzles(text=puzzle_text)
@@ -38,18 +57,45 @@ def save_answer_by_puzzle_text(answer_text, puzzle_text):
         answer = Answers(text=answer_text, puzzleId = puzzle.id)
         db.session.add(answer)
         db.session.commit()   
-        return
+        return "Saved as new puzzle and answer!"
     if (len(puzzles) > 1):
         #something is wrong
-        return
+        return "Something is wrong! There was more than one puzzle matched."
     else:
+        #make sure this answer doesn't exist.
+        #answers = Answers.query.where(Answers.text == answer_text).all()
+        sql = f"select * from answers where answers.text = \"{answer_text}\";"
+        answers = db.session.execute(text(sql)).all()
+        if(len(answers) != 0):
+            return "Answer already exists.  Not adding it."
+        #make sure it is a valid answer.
+        for letter in answer_text:
+            if (letter not in puzzle_text):
+                return "Not a vaid answer...  it has letters not in the puzzle. (extra spaces maybe?)"
+
         #save this answer with this puzzle id
-        answer = Answers(text=answer_text, puzzleId = puzzles[0].id)
-        db.session.add(answer)
-        db.session.commit()  
-        return
+        #answer = Answers(text=answer_text, puzzleId = puzzles[0].id)
+        sql = f"INSERT INTO answers (puzzleId, text) VALUES ({puzzles[0].id}, \"{answer_text}\")"
+        db.session.execute(text(sql))
+        db.session.commit()        
+        return "Added answer to existing puzzle."
     
-def get_answers():
-    sql = "SELECT answers.text as answer, puzzles.text as puzzle, puzzles.id FROM answers left join puzzles on puzzles.id = answers.puzzleId;"
+def get_all_answers():
+    sql = "SELECT answers.id as id, answers.text as answer, puzzles.text as puzzle, puzzles.id as puzzleId FROM answers left join puzzles on puzzles.id = answers.puzzleId order by puzzles.id;"
     return db.session.execute(text(sql)).all()
 
+def get_answers(puzzleId):
+    sql = f"SELECT puzzles.id as puzzleId, answers.id as answerId, puzzles.text as puzzle, answers.text as answer FROM answers left join puzzles on puzzles.id = answers.puzzleId where puzzles.id = {puzzleId} order by puzzles.id;"
+    return db.session.execute(text(sql)).all()
+
+def get_results():
+    sql = "select puzzleId, answerId, puzzles.text, answers.text, count(answerId) from votes left join answers on answers.id = votes.answerId left join puzzles on puzzles.id = puzzleId group by answerId;"
+    return db.session.execute(text(sql)).all()
+
+def get_results(puzzleId):
+    sql = f"select puzzleId, answerId, puzzles.text, answers.text, count(answerId) from votes left join answers on answers.id = votes.answerId left join puzzles on puzzles.id = puzzleId where puzzleId = {puzzleId} group by answerId;"
+    return db.session.execute(text(sql)).all()
+
+def get_puzzles_with_answers():
+    sql = "select distinct(puzzles.id), puzzles.text from puzzles join answers on puzzles.id = answers.puzzleId;"
+    return db.session.execute(text(sql)).all()
